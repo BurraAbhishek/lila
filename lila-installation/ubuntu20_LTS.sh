6dc554fa062d5e35e93d6e215e315a5760400281
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2022-11-26 22:29:15 +0530

    Update ubuntu20_LTS.sh

--- lila-installation/ubuntu20_LTS.sh
+++ lila-installation/ubuntu20_LTS.sh
@@ -1,6 +1,9 @@
+# Run this first and restart your terminal!
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 sudo apt update
-sudo apt install git
+sudo apt install -y git
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
+cd
 mkdir dev
 cd dev
 mkdir lichess
@@ -8,18 +11,17 @@ cd lichess
 git clone --recursive https://github.com/lichess-org/lila.git
 git clone --recursive https://github.com/lichess-org/lila-ws.git
 git clone --recursive https://github.com/lichess-org/lila-db-seed.git
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 cd
 jabba install openjdk@1.17.0
-sudo apt install python3-pip
+sudo apt install -y python3-pip
 nvm install node
 npm install --global yarn
-sudo apt install redis-server
+sudo apt install -y redis-server
 echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
 echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
 curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
 sudo apt-get update
-sudo apt-get install sbt
+sudo apt-get install -y sbt
 wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
