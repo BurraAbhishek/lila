0033f925b08ec505f32b5006e1ce4aa67a82e9d3
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2022-12-03 19:11:41 +0530

    Update ubuntu20_LTS.sh

--- lila-installation/ubuntu20_LTS.sh
+++ lila-installation/ubuntu20_LTS.sh
@@ -3,6 +3,8 @@
 # Do NOT use sudo to run this. Just run this normally, and enter password when prompted
 # example: use: ./lila.sh, not sudo ./lila.sh if the file is called lila.sh
 home_directory=$HOME
+nvm install node
+npm install --global yarn
 sudo apt update
 sudo apt install -y git
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
@@ -17,8 +19,6 @@ git clone --recursive https://github.com/lichess-org/lila-db-seed.git
 cd $home_directory
 jabba install openjdk@1.17.0
 sudo apt install -y python3-pip
-nvm install node
-npm install --global yarn
 sudo apt install -y redis-server
 echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
 echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
@@ -27,8 +27,6 @@ sudo apt-get update
 sudo apt-get install -y sbt
 wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
-wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
-sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2_amd64.deb
 sudo apt update
 sudo apt install -y mongodb-org
 sudo systemctl start mongod
