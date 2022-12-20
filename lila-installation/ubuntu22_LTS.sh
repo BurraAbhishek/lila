bade06c933652842d64e8c801282083f5ac3f7bd
Burra Abhishek <76472381+BurraAbhishek@users.noreply.github.com>
2022-12-20 21:23:41 +0530

    Replace sbt with coursier, yarn with pnpm

--- lila-installation/ubuntu22_LTS.sh
+++ lila-installation/ubuntu22_LTS.sh
@@ -4,7 +4,7 @@
 # example: use: ./lila.sh, not sudo ./lila.sh if the file is called lila.sh
 home_directory=$HOME
 nvm install node
-npm install --global yarn
+npm i -g pnpm
 sudo apt update
 sudo apt install -y git
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
@@ -19,12 +19,10 @@ git clone --recursive https://github.com/lichess-org/lila-db-seed.git
 cd $home_directory
 jabba install openjdk@1.17.0
 sudo apt install -y python3-pip
+curl -fL https://github.com/coursier/launchers/raw/master/cs-x86_64-pc-linux.gz | gzip -d > cs
+chmod +x cs
+./cs setup
 sudo apt install -y redis-server
-echo "deb https://repo.scala-sbt.org/scalasbt/debian all main" | sudo tee /etc/apt/sources.list.d/sbt.list
-echo "deb https://repo.scala-sbt.org/scalasbt/debian /" | sudo tee /etc/apt/sources.list.d/sbt_old.list
-curl -sL "https://keyserver.ubuntu.com/pks/lookup?op=get&search=0x2EE0EA64E40A89B84B2DF73499E82A75642AC823" | sudo apt-key add
-sudo apt-get update
-sudo apt-get install -y sbt
 wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2_amd64.deb
